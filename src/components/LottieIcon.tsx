e7a2fc46886e0db7823f4a712f9341da6608bfc5
Emmanuel Charley Raluswinga <105504810+Tshikwetamakole@users.noreply.github.com>
2025-07-11 13:55:16 +0200

    Update LottieIcon.tsx

--- src/components/LottieIcon.tsx
+++ src/components/LottieIcon.tsx
@@ -1,1 +1,18 @@
+import React from "react";
+import Lottie from "lottie-react";
 
+interface LottieIconProps {
+  animation: object;
+  className?: string;
+  loop?: boolean;
+}
+
+const LottieIcon: React.FC<LottieIconProps> = ({
+  animation,
+  className = "w-24 h-24",
+  loop = true,
+}) => {
+  return <Lottie animationData={animation} loop={loop} className={className} />;
+};
+
+export default LottieIcon;
