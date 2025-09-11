0f0035d1e3f98ed7ec904d012b10848fd0873754
Emmanuel Charley Raluswinga <105504810+Tshikwetamakole@users.noreply.github.com>
2025-09-11 18:34:01 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Use relative paths so assets load correctly on custom domains
-export default defineConfig({
-  base: './', // important: generates ./assets/... not /<repo>/assets/...
-  plugins: [react()],
-})
