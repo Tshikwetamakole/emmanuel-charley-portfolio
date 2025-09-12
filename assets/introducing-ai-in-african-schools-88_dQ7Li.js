8a7c106941901c716514878f3ae173e6649c0724
Muano Tshikwetamakole <victorraluswinga@gmail.com>
2025-09-12 13:43:37 +0200

    Updates

---
+++ assets/introducing-ai-in-african-schools-88_dQ7Li.js
@@ -0,0 +1,9 @@
+const n=`---\r
+title: "Introducing AI in African Schools"\r
+date: "2024-02-16"\r
+---\r
+\r
+AI is coming to Africa’s classrooms — but is it truly ready?\r
+\r
+In this article, we explore the potential of machine learning in improving education and how policymakers, parents, and learners can prepare...\r
+`;export{n as default};
