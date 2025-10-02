f9d0851d686df66a5cfc552b83b3543a4bfb450c
Tshikwetamakole <Tshikwetamakole@users.noreply.github.com>
2025-10-02 18:48:05 +0000

    deploy: 6b11f8168a3db80c95196a8ca121ff805c6cc5ea

--- assets/introducing-ai-in-african-schools-BllVeVtH.js
+++ assets/introducing-ai-in-african-schools-BllVeVtH.js
@@ -6,4 +6,6 @@ date: "2024-02-16"
 AI is coming to Africa’s classrooms — but is it truly ready?
 
 In this article, we explore the potential of machine learning in improving education and how policymakers, parents, and learners can prepare...
+
+[Read the full article here](https://www.charleyraluswinga.space/2024/02/introducing-ai-in-african-schools-great.html)
 `;export{n as default};
